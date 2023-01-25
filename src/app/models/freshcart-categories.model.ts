635a55afce4d28ef6577172f96dfd2ba9eec2d40
Usshel <pawelopadczuk@gmail.com>
2023-01-25 12:17:34 +0100

    feat: category, product, store model created

---
+++ src/app/models/freshcart-categories.model.ts
@@ -0,0 +1,5 @@
+export interface FreshcartCategoriesModel {
+  readonly id: string;
+  readonly imageUrl: string;
+  readonly name: string;
+}
---
+++ src/app/models/freshcart-stores.model.ts
@@ -0,0 +1,7 @@
+export interface FreshcartStoresModel {
+  readonly name: string;
+  readonly logoUrl: string;
+  readonly distanceInMeters: number;
+  readonly tagIds: string[];
+  readonly id: string;
+}
