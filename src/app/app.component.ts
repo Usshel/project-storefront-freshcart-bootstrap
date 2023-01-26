a27795b8c0c124c07e727a6125bcbe681feff25a
Usshel <pawelopadczuk@gmail.com>
2023-01-27 00:52:08 +0100

    feat: method and subject servicing navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { FreshcartCategoriesModel } from './models/freshcart-categories.model';
 import { FreshcartStoresModel } from './models/freshcart-stores.model';
 import { CategoriesService } from './services/categories.service';
@@ -14,8 +14,23 @@ export class AppComponent {
   title = 'ng-freshcard-bootstrap-theme';
   readonly categories$: Observable<FreshcartCategoriesModel[]> = this._categoriesService.getAllFreshCartCategories();
   readonly stores$: Observable<FreshcartStoresModel[]> = this._storesService.getAllFreshCartStores();
-  readonly getToKnowUs$: Observable<string[]> = of(['Company','About','Blog','Help Center','Our Value']) ;
-
+  readonly getToKnowUs$: Observable<string[]> = of(['Company', 'About', 'Blog', 'Help Center', 'Our Value']);
+  private _isMenuOpenSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public isMenuOpen$: Observable<boolean> = this._isMenuOpenSubject.asObservable();
+  
   constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) {
   }
+  onMenuOpen(isOpen:boolean): void {
+    if(isOpen === false) {
+      this._isMenuOpenSubject.next(false)
+    }else
+    {
+      this._isMenuOpenSubject.next(true)
+    }
+    
+  }
+
+  
+
 }
+
