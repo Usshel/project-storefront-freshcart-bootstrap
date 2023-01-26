e6331398c42f304c267444267950608e78459c45
Usshel <pawelopadczuk@gmail.com>
2023-01-27 00:50:20 +0100

    feat: categories Observable added feat: stores Observable added feat: geToKnowUs Observable added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { FreshcartCategoriesModel } from './models/freshcart-categories.model';
+import { FreshcartStoresModel } from './models/freshcart-stores.model';
+import { CategoriesService } from './services/categories.service';
+import { StoresService } from './services/stores.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-freshcard-bootstrap-theme';
+  readonly categories$: Observable<FreshcartCategoriesModel[]> = this._categoriesService.getAllFreshCartCategories();
+  readonly stores$: Observable<FreshcartStoresModel[]> = this._storesService.getAllFreshCartStores();
+  readonly getToKnowUs$: Observable<string[]> = of(['Company','About','Blog','Help Center','Our Value']) ;
+
+  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) {
+  }
 }
