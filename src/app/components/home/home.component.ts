6c0601e7c2403175e4cbd380c0d20fb465385c01
Usshel <pawelopadczuk@gmail.com>
2023-01-27 16:15:03 +0100

    feat: store and categories observables added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from '../../models/freshcart-categories.model';
+import { StoreModel } from '../../models/freshcart-stores.model';
+import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +13,9 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAllFreshCartCategories();
+  readonly stores$: Observable<StoreModel[]> = this._storesService.getAllFreshCartStores();
+
+  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) {
+  }
 }
