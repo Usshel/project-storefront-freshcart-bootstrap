7e657b8aaa26f5e7c0c493ef64489330d217825c
Usshel <pawelopadczuk@gmail.com>
2023-01-27 01:21:42 +0100

    fix: type names changed

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FreshcartCategoriesModel } from '../models/freshcart-categories.model';
+import { CategoryModel } from '../models/freshcart-categories.model';
 
 @Injectable({ providedIn: 'root' })
 export class CategoriesService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAllFreshCartCategories(): Observable<FreshcartCategoriesModel[]> {
-    return this._httpClient.get<FreshcartCategoriesModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories');
+  getAllFreshCartCategories(): Observable<CategoryModel[]> {
+    return this._httpClient.get<CategoryModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories');
   }
 
-  getAllFreshCartCategoriesById(id: string): Observable<FreshcartCategoriesModel> {
-    return this._httpClient.get<FreshcartCategoriesModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories/${id}`);
+  getAllFreshCartCategoriesById(id: string): Observable<CategoryModel> {
+    return this._httpClient.get<CategoryModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories/${id}`);
   }
 }
--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FreshcartStoresModel } from '../models/freshcart-stores.model';
+import { StoreModel } from '../models/freshcart-stores.model';
 
 @Injectable({ providedIn: 'root' })
 export class StoresService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAllFreshCartStores(): Observable<FreshcartStoresModel[]> {
-    return this._httpClient.get<FreshcartStoresModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
+  getAllFreshCartStores(): Observable<StoreModel[]> {
+    return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
   }
 
-  getAllFreshCartStoresById(id: string): Observable<FreshcartStoresModel> {
-    return this._httpClient.get<FreshcartStoresModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores/${id}`);
+  getAllFreshCartStoresById(id: string): Observable<StoreModel> {
+    return this._httpClient.get<StoreModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores/${id}`);
   }
 }
