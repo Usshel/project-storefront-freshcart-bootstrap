ee5a0d1e5abe852054ed30d61d28714f82fe71c7
Usshel <pawelopadczuk@gmail.com>
2023-01-27 01:14:33 +0100

    fix: model name in methods changed

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FreshcartProductsModel } from '../models/freshcart-products.model';
+import { ProductModel } from '../models/freshcart-products.model';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAllFreshCartProducts(): Observable<FreshcartProductsModel[]> {
-    return this._httpClient.get<FreshcartProductsModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-products');
+  getAllFreshCartProducts(): Observable<ProductModel[]> {
+    return this._httpClient.get<ProductModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-products');
   }
 }
