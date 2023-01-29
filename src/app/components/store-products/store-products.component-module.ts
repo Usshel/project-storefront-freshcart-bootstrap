fc275abbb92a417e1718e78d23621a10ca284767
Usshel <pawelopadczuk@gmail.com>
2023-01-29 14:41:30 +0100

    feat:imports added

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   declarations: [StoreProductsComponent],
   providers: [],
   exports: [StoreProductsComponent]
