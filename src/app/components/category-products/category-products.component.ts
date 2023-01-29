764d6742337738977f8ffc5cd6c445defface3c8
Usshel <pawelopadczuk@gmail.com>
2023-01-29 22:37:43 +0100

    feat: priceFilter added to productsSorted

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, combineLatest, of } from 'rxjs';
-import { filter, map, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, filter, map, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/freshcart-categories.model';
 import { StoreModel } from '../../models/freshcart-stores.model';
 import { ProductModel } from '../../models/freshcart-products.model';
@@ -20,11 +20,12 @@ import { StoresService } from '../../services/stores.service';
 export class CategoryProductsComponent {
   //hardCoded sort options
   readonly sort$: Observable<string[]> = of(['Featured', 'Price: Low to High', 'Price: High to Low', 'Avg.rating'])
-  
 
+  //sort form
   readonly sort: FormGroup = new FormGroup({ sortOption: new FormControl() });
 
-
+  //filter price form
+  readonly filterPrice: FormGroup = new FormGroup({ priceFrom: new FormControl(), priceTo: new FormControl() });
 
 
   //all categories
@@ -34,60 +35,77 @@ export class CategoryProductsComponent {
     );
 
 
+
   //category id from http params
   readonly categoryIdParams$: Observable<string> = this._activatedRoute.params.pipe(
     map((params) => params['categoryId']),
     shareReplay(1)
   )
 
+  onFilterPriceSubmitted(filterPrice: FormGroup): void {
+  }
+
   //category for particular site by params
   readonly categorySite$: Observable<CategoryModel[]> = combineLatest([
     this.categories$,
     this.categoryIdParams$,
-    
-  ]).pipe(map(([categories, categoryParamsId]) => 
-  categories.filter(category => category.id == categoryParamsId)))
+  ]).pipe(map(([categories, categoryParamsId]) =>
+    categories.filter(category => category.id == categoryParamsId)))
+
 
   // all stores
   readonly stores$: Observable<StoreModel[]> = this._storesService.getAllFreshCartStores();
 
 
 
-  //all Products by params
+  //all Products by Category
   readonly products$: Observable<ProductModel[]> = combineLatest([
     this._productsService.getAllFreshCartProducts(),
     this.categoryIdParams$,
   ]).pipe(map(([products, categoryParsId]) =>
     products.filter((product) => product.categoryId == categoryParsId)
-    ));
+  ));
 
-
-    readonly productsSorted$: Observable<ProductModel[]> = combineLatest([
-      this.products$,
-      this.sort.valueChanges.pipe(
-        startWith(''))
-    ]).pipe(
-      map(([products, sortForm]) => 
-      products.sort((productA, productB) => {
-        if(sortForm.sortOption === 'Featured'){
-          if(productA.featureValue > productB.featureValue) return sortForm.sortOption == 'Featured' ? -1 : 1;
+  //Products SortedAndFilteredByPrice
+  readonly productsSorted$: Observable<ProductModel[]> = combineLatest([
+    this.products$,
+    this.sort.valueChanges.pipe(
+      startWith('')),
+    this.filterPrice.valueChanges.pipe(
+      debounceTime(3000),
+      startWith(
+        {priceFrom: 1, priceTo: 1000}
+        
+      ))
+  ]).pipe(
+    map(([products, sortForm, priceF]) => 
+      products.filter(product => 
+        priceF.priceFrom && priceF.priceTo ? product.price >= priceF.priceFrom && product.price < priceF.priceTo : 
+        priceF.priceFrom ? product.price >= priceF.priceFrom :
+        priceF.priceTo ? product.price <= priceF.priceTo :
+        product
+        )
+      .sort((productA, productB) => {
+        if (sortForm.sortOption === 'Featured') {
+          if (productA.featureValue > productB.featureValue) return sortForm.sortOption == 'Featured' ? -1 : 1;
         }
-        if(sortForm.sortOption === 'Price: Low to High' || sortForm.sortOption === 'Price: High to Low'  ){
-          if(productA.price > productB.price) return sortForm.sortOption == 'Price: Low to High' ? 1 : -1;
-          if(productA.price < productB.price) return sortForm.sortOption == 'Price: High to Low' ? 1 : -1;
+        if (sortForm.sortOption === 'Price: Low to High' || sortForm.sortOption === 'Price: High to Low') {
+          if (productA.price > productB.price) return sortForm.sortOption == 'Price: Low to High' ? 1 : -1;
+          if (productA.price < productB.price) return sortForm.sortOption == 'Price: High to Low' ? 1 : -1;
         }
-        if(sortForm.sortOption === 'Avg.rating'){
-          if(productA.ratingValue > productB.ratingValue) return sortForm.sortOption == 'Avg.rating' ? -1 : 1;
+        if (sortForm.sortOption === 'Avg.rating') {
+          if (productA.ratingValue > productB.ratingValue) return sortForm.sortOption == 'Avg.rating' ? -1 : 1;
         }
         return 0
       }
-      ))
+      )
     )
+  )
 
   constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService, private _storesService: StoresService) {
   }
-  
-  
+
+
 
 
 }
