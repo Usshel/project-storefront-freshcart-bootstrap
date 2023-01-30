19ef9dc92ecb87de0180cd88dba686a297c674b1
Usshel <pawelopadczuk@gmail.com>
2023-01-30 19:59:25 +0100

    import added

--- src/app/components/category-products/category-products.component-module.ts
+++ src/app/components/category-products/category-products.component-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { CategoryProductsComponent } from './category-products.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [CategoryProductsComponent],
   providers: [],
   exports: [CategoryProductsComponent]
