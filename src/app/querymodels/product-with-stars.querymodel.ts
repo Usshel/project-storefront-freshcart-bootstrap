8d39ee6ddba26d3c91e0472a04e043b979385f1b
Usshel <pawelopadczuk@gmail.com>
2023-01-31 13:14:29 +0100

    feat: created a product queryModel with stars

---
+++ src/app/querymodels/product-with-stars.querymodel.ts
@@ -0,0 +1,11 @@
+export interface ProductWithStarsQuerymodel {
+  readonly categoryId: string;
+  readonly featureValue: number;
+  readonly id: string;
+  readonly imageUrl: string;
+  readonly name: string;
+  readonly price: number;
+  readonly ratingCount: number;
+  readonly ratingValue: number[];
+  readonly storeIds: string[];
+}
